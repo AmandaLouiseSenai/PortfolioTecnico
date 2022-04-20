faeb6de53bab81e8ed63b67dcb4b049fa4d12f3d
AmandaLouiseSenai <83764940+AmandaLouiseSenai@users.noreply.github.com>
2022-04-20 11:44:26 -0300

    Create Atividade de Alterações.sql

---
+++ Banco de Dados/Atividade de Alterações.sql
@@ -0,0 +1,11 @@
+1 alter table cliente add column idade int;
+2 alter table fornecedores add column origem  varchar;
+3 alter table funcionarios add column genero varchar;
+4 alter table vendas add column frete varchar;
+5 alter table produtos add column cupom_de_descontos varchar;
+
+1 Adicionar idade para ter um controle, para facilitar o envio de recomendações.
+2 Adicionar origem para saber da onde o produto vem.
+3 Adicionar género, para ter um conhecimento do publico.
+4 Adicionar frete, para saber o valor estimado para enviar a compra para cada lugar.
+5 Adicionar cupom de desconto, porque o desconto é sempre bom.
